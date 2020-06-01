93ebc54648dd76b2eefa46eb439678ba64310300
marceloarb <59586505+marceloarb@users.noreply.github.com>
2020-06-01 14:48:24 -0500

    Update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Counter from './Components/Counter';
 
-function App() {
+const App = ()=> {
   return (
     <div className="App">
       <header className="App-header">
         <h2>Counter App</h2>
       </header>
+      
+       
       <Counter/>
     </div>
   );
--- src/Components/ButtonPad.jsx
+++ src/Components/ButtonPad.jsx
@@ -3,8 +3,16 @@ import React from 'react'
 export default function ButtonPad() {
     return (
         <div>
-            <button>Up Button</button>
-            <button>Down Button</button>
+            <button><svg className="bi bi-plus-circle" width="8em" height="8em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+    <path fill-rule="evenodd" d="M8 3.5a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-.5.5H4a.5.5 0 0 1 0-1h3.5V4a.5.5 0 0 1 .5-.5z"/>
+    <path fill-rule="evenodd" d="M7.5 8a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0V8z"/>
+    <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+  </svg></button>
+            <button><svg className="bi bi-dash-circle" width="8em" height="8em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+  <path fill-rule="evenodd" d="M3.5 8a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.5-.5z"/>
+</svg></button>
+            
         </div>
     )
 }
